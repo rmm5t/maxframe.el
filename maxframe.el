27beb9a59a33a3a7e2de1db63c5140fad6ba4f4d
Ryan McGeary <ryanongit@mcgeary.org>
2010-01-26 12:29:03 -0500

    Removed comment about limitations that no longer exist in Emacs 22+

--- maxframe.el
+++ maxframe.el
@@ -47,14 +47,6 @@
 ;; Requires Emacs 22 (for fringe support), but maximize-frame still works
 ;; under Emacs 21 on Windows.
 ;;
-;; Emacs does not recognize when the display's resolution is changed. This is
-;; a problem because I would like to be able to re-maximize the frame after
-;; connecting to a display with different resolution. Unfortunately,
-;; display-pixel-width and display-pixel-height yield the display resolution
-;; values from when emacs was started instead of the current display
-;; values. Perhaps there's a way to have emacs re-sniff these values, but I'm
-;; not yet sure how.
-;;
 ;; Credits
 ;; -------
 ;; The w32 specific functions were borrowed from the Emacs Manual:
